8b6a173ec889a6994f5251b45d668f44a729f603
Levent Ali <lebreeze@gmail.com>
2010-04-09 22:53:53 +0100

    html5 layout

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,2 +1,6 @@
 class PhotosController < ApplicationController
+  
+  # the homepage
+  def index
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Sequester::Application.routes.draw do |map|
+  resources :photos
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +49,7 @@ Sequester::Application.routes.draw do |map|
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "photos#index"
 
   # See how all your routes lay out with "rake routes"
 
