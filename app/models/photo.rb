04c55351f9f27b9730f133147387d7c7e45c35e8
Levent Ali <lebreeze@gmail.com>
2010-04-30 15:52:05 +0100

    unique title

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,6 +1,7 @@
 class Photo < ActiveRecord::Base
   is_sluggable :title
   validates_presence_of :url
+  validates_uniqueness_of :title
   default_scope :order => "created_at DESC"
   
   def thumb_url
