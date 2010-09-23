ba2d53aada4c5e07494d61602387457f653c2a89
Levent Ali <lebreeze@gmail.com>
2010-09-23 22:32:52 +0100

    Revert "Try page caching on heroku"
    This reverts commit 755b1de431d0d248ed4b9e0204645ec488ecf0e1.

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,8 +3,6 @@ class PhotosController < ApplicationController
   before_filter :authenticate_admin!, :only => [:new, :create, :edit, :list, :update]
   before_filter :load_photo, :only => [:edit, :update, :show, :up, :down]
 
-  caches_page :show
-
   def up
     @photo.move_higher
     redirect_to photos_url
@@ -19,7 +17,6 @@ class PhotosController < ApplicationController
   end
   
   def update
-    expire_page :action => :show
     if @photo.update_attributes(params[:photo])
       redirect_to photos_url
     else
@@ -32,7 +29,6 @@ class PhotosController < ApplicationController
   end
 
   def create
-    expire_page :action => :show
     @photo = Photo.new(params[:photo])
     if @photo.save
       redirect_to photos_path
